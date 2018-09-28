6fbd9ebf835146a04235b1d113c267b2b8e7d130
aaron.buckley@springernature.com <aaron.buckley@springernature.com>
2018-09-28 16:42:20 +0100

    adding watch instructions to readme

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Make sure you are on the latest nightly build. This will change once Rust v1.30.
 To do this run `rustup override set nightly`
 
 ## Running
-Simply run `cargo build` to build the solution and `cargo run` to fire it up
+Simply run `cargo build` to build the solution and `cargo run` to fire it up. Alternatively you can run `cargo watch -x run`, this will force cargo to watch for file changes, compile and run the new build.
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate core;
+
+use rocket::http::RawStr;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<username>/<age>")]
+fn user_details(username: &RawStr, age: u8) -> String {
+    format!("Hello, world! {}, {}", username, age)
+}
+
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/<username>/<age>", routes![user_details]);
 }
